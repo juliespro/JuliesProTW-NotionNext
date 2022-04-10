6ac2ca00cf794b95698e64816515c22a6c2d52d2
txs <txshon@gmail.com>
2022-04-11 07:51:52 +0800

    fix target blank in react issue

--- components/FacebookFanPage.js
+++ components/FacebookFanPage.js
@@ -5,10 +5,15 @@ import { FacebookIcon } from 'react-share'
 const FacebookPage = () => (
   <div className="shadow-md hover:shadow-xl dark:text-gray-300 border dark:border-black rounded-xl px-2 py-4 bg-white dark:bg-hexo-black-gray lg:duration-100 justify-center">
     <div className="flex  items-center pb-2">
-      <a href={BLOG.FACEBOOK_PAGE} target="_blank" className="p-1 pr-2 pt-0">
+      <a
+        href={BLOG.FACEBOOK_PAGE}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-1 pr-2 pt-0"
+      >
         <FacebookIcon size={28} round />
       </a>
-      <a href={BLOG.FACEBOOK_PAGE} target="_blank">
+      <a href={BLOG.FACEBOOK_PAGE} rel="noopener noreferrer" target="_blank">
         FACEBOOK 粉絲團
       </a>
     </div>
